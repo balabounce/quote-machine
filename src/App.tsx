48cc9587fcb14d5d6f318a771310f080aedc7dec
Balabounce <yoba.number.one@gmail.com>
2021-08-24 21:05:23 +0300

    made the casdace - form, text, img

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import QuoteBox from './components/quoteBox.component';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <QuoteBox/>
+      
   );
 }
 
